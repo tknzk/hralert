486abd5a29888717d0d7d460619ac25ffeb13890
tknzk <tkm.knzk@gmail.com>
2018-10-05 01:47:39 +0900

    obey rubocop

--- 01a.rb
+++ 01a.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'oauth2'
 
 client_id = ENV['FITBIT_CLIENT_ID']
@@ -5,8 +7,10 @@ client_secret = ENV['FITBIT_CLIENT_SECRET']
 
 redirect_uri = 'http://localhost:3002/callback'
 
-client = OAuth2::Client.new(client_id, client_secret, site: 'https://api.fitbit.com', authorize_url: 'https://www.fitbit.com/oauth2/authorize', token_url: 'https://api.fitbit.com/oauth2/token')
+client = OAuth2::Client.new(client_id, client_secret,
+                            site: 'https://api.fitbit.com',
+                            authorize_url: 'https://www.fitbit.com/oauth2/authorize',
+                            token_url: 'https://api.fitbit.com/oauth2/token')
 
 # generate authorized_url
 puts client.auth_code.authorize_url(redirect_uri: redirect_uri, scope: 'heartrate')
-
--- daily.rb
+++ daily.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'oauth2'
 require 'base64'
 require 'redis'
@@ -8,42 +10,35 @@ require 'rest-client'
 client_id = ENV['FITBIT_CLIENT_ID']
 client_secret = ENV['FITBIT_CLIENT_SECRET']
 
-if ENV['REDIS_URL'] != nil
+if !ENV['REDIS_URL'].nil?
   uri   = URI.parse ENV['REDIS_URL']
-  redis = Redis.new:host => uri.host, :port => uri.port, :password => uri.password
+  redis = Redis.new host: uri.host, port: uri.port, password: uri.password
 else
-  redis = Redis.new host:"127.0.0.1", port:"6379"
+  redis = Redis.new host: '127.0.0.1', port: '6379'
 end
 
 refresh_token = redis.get 'refresh_token'
 
-if refresh_token.nil?
-  refresh_token = ARGV[0]
-end
+refresh_token = ARGV[0] if refresh_token.nil?
 
-
-client = OAuth2::Client.new( client_id, client_secret,
+client = OAuth2::Client.new(client_id, client_secret,
                             site: 'https://api.fitbit.com',
                             authorize_url: 'https://www.fitbit.com/oauth2/authorize',
-                            token_url: 'https://api.fitbit.com/oauth2/token'
-                           )
+                            token_url: 'https://api.fitbit.com/oauth2/token')
 
 bearer_token = "#{client_id}:#{client_secret}"
 encoded_bearer_token = Base64.strict_encode64(bearer_token)
-
 
 # get access_token from refresh_token
 auth_token = OAuth2::AccessToken.new(
   client,
   refresh_token,
-  {
-    :refresh_token => refresh_token,
-    :expires_at => 3600,
-    :scope => 'heartrate',
-  }
+  refresh_token: refresh_token,
+  expires_at: 3600,
+  scope: 'heartrate'
 )
 
-access_token = auth_token.refresh!({:headers =>{'Authorization' => "Basic #{encoded_bearer_token}" }})
+access_token = auth_token.refresh!(headers: { 'Authorization' => "Basic #{encoded_bearer_token}" })
 hash = access_token.to_hash
 
 # save refresh_token
@@ -53,11 +48,9 @@ redis.set 'refresh_token', hash[:refresh_token]
 token = OAuth2::AccessToken.new(
   client,
   hash[:access_token],
-  {
-    :refresh_token => refresh_token,
-    :expires_at => 3600,
-    :scope => 'heartrate',
-  }
+  refresh_token: refresh_token,
+  expires_at: 3600,
+  scope: 'heartrate'
 )
 
 target = Date.today - 1
@@ -74,16 +67,19 @@ result = JSON.parse(response.body)
 activities_heart_intraday = result['activities-heart-intraday']
 mackerel = {}
 activities_heart_intraday['dataset'].each do |data|
-  epoch = Time.parse("#{target.strftime("%Y-%m-%d")} #{data['time']}").to_i
+  epoch = Time.parse("#{target.strftime('%Y-%m-%d')} #{data['time']}").to_i
   mackerel = [{
     name:  'heartbeat',
     time:  epoch,
-    value: data['value'],
+    value: data['value']
   }]
   puts mackerel.inspect
   begin
-    RestClient.post "https://mackerel.io/api/v0/services/heartrate/tsdb", mackerel.to_json, 'Content-Type' => 'application/json', 'X-Api-Key' => ENV['MACKEREL_API_KEY']
-  rescue => e
+    RestClient.post('https://mackerel.io/api/v0/services/heartrate/tsdb',
+                    mackerel.to_json,
+                    'Content-Type' => 'application/json',
+                    'X-Api-Key' => ENV['MACKEREL_API_KEY'])
+  rescue StandardError => e
     puts e.message
   end
 end
